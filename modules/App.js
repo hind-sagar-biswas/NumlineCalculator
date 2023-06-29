beb5fd32f16130aa2a504d9f3b8f36bb54eb35ea
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-06-29 12:30:30 +0600

    added all the modules

--- modules/App.js
+++ modules/App.js
@@ -1,10 +1,12 @@
-import Numberline from "./Numberline.js";
+import Form from "./Form.js";
 import Operator from "./Operator.js";
+import Numberline from "./Numberline.js";
 import ParseExpression from "./ParseExpression.js";
 
 
 window.App = {
-    numberline: Numberline,
+    form: Form,
     operator: Operator,
+    numberline: Numberline,
     parseExp: ParseExpression,
 }
--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,70 @@
+const formFields = [
+	{
+		type: "number",
+		name: "left",
+		value: 1,
+		classList: ["form-control", "mb-2"],
+	},
+	{
+		type: "select",
+		name: "operator",
+		classList: ["form-control", "mb-2"],
+		options: [
+			{
+				name: "+",
+				value: "add",
+			},
+			{
+				name: "-",
+				value: "sub",
+			},
+			{
+				name: "*",
+				value: "mul",
+			},
+			{
+				name: "/",
+				value: "div",
+			},
+		],
+	},
+	{
+		type: "number",
+		name: "right",
+		classList: ["form-control", "mb-2"],
+		value: 1,
+	},
+	{
+		type: "submit",
+		name: "calculate",
+		name: "submit",
+		classList: ["btn", "btn-primary"],
+	},
+];
+
 window.addEventListener("load", () => {
-	const numberline = new App.numberline();
-	numberline.render();
+	const form = new App.form({
+		structure: "stack",
+	});
+	form.setEvents({
+		submit: (e) => {
+			e.preventDefault();
+			const inputs = [...e.target];
+			const left = parseInt(inputs[0].value);
+			const operator = inputs[1].value;
+			const right = parseInt(inputs[2].value);
+			
+			const parser = new App.parseExp(left, right, operator);
+			const parsedExp = parser.parse();
+			console.log(parsedExp);
+
+			const numberline = new App.numberline({
+				min: parsedExp.limit[0],
+				max: parsedExp.limit[1],
+			});
+			numberline.render();
+		},
+	});
+	form.setFields(formFields);
+	form.render("form-container");
 });
