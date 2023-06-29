10e85b4eaa7eb8264e1dbe274ef89713a98f1e3b
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-06-29 12:29:53 +0600

    created parser

--- modules/ParseExpression.js
+++ modules/ParseExpression.js
@@ -3,54 +3,84 @@ export default class ParseExpression {
 		this.left = left;
 		this.right = right;
 		this.operator = generic_operator;
-
-		return this.#parse();
 	}
 
-	#parse() {
-        switch (this.operator) {
-            case 'add':
-                return this.#add();
-            case 'sub':
-                return this.#sub();
-            case 'mul':
-                return this.#mul();
-            case 'div':
-                return this.#div();
-        
-            default:
-                return null;
-        }
-    }
+	parse() {
+		switch (this.operator) {
+			case "add":
+				return this.#add();
+			case "sub":
+				return this.#sub();
+			case "mul":
+				return this.#mul();
+			case "div":
+				return this.#div();
+
+			default:
+				return null;
+		}
+	}
 
 	#add() {
-        return {
+		const sum = this.left + this.right;
+		let lim = [-10, 10];
+		lim[0] = sum > 0 ? -1 * sum - 3 : sum - 3;
+		lim[1] = sum < 0 ? -1 * sum + 3 : sum + 3;
+
+		return {
 			initial: this.left,
 			by: this.right,
+			limit: lim,
 		};
-    }
-    #sub() {
-        return {
+	}
+	#sub() {
+		const dif = this.left - this.right;
+		let lim = [-10, 10];
+		lim[0] = dif > 0 ? -1 * dif - 3 : dif - 3;
+		lim[1] = dif < 0 ? -1 * dif + 3 : dif + 3;
+
+		return {
 			initial: this.left,
 			by: -1 * this.right,
+			limit: lim,
 		};
-    }
-    #mul() {
-        return {
+	}
+	#mul() {
+		const pro = this.left * this.right;
+		let lim = [-10, 10];
+		lim[0] = pro > 0 ? -1 * pro - 3 : pro - 3;
+		lim[1] = pro < 0 ? -1 * pro + 3 : pro + 3;
+
+		let rotation = 0;
+		if (this.left < 0) rotation++;
+		if (this.right < 0) rotation++;
+
+		return {
 			initial: 0,
 			final: null,
-			by: 1,
-			times: 1,
-			rotation: 0,
+			by: Math.abs(this.right),
+			times: Math.abs(this.left),
+			rotation,
+			limit: lim,
 		};
-    }
-    #div() {
-        return {
+	}
+	#div() {
+		const qou = this.left / this.right;
+		let lim = [-10, 10];
+		lim[0] = qou > 0 ? -1 * qou - 3 : qou - 3;
+		lim[1] = qou < 0 ? -1 * qou + 3 : qou + 3;
+
+		let rotation = 0;
+		if (this.left < 0) rotation++;
+		if (this.right < 0) rotation++;
+
+		return {
 			initial: 0,
-			final: null,
-			by: 1,
-			times: 1,
-			rotation: 0,
+			final: Math.abs(this.left),
+			by: Math.abs(this.right),
+			times: null,
+			rotation,
+			limit: lim,
 		};
-    }
+	}
 }
